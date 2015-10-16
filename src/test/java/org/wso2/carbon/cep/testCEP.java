ae3bcf3a25786373c30e6f8b6ddcd06b93cd07cc
tishan89 <dtishanpubudu@gmail.com>
2015-10-16 15:32:53 +0530

    adding synchronous testcase

--- src/test/java/org/wso2/carbon/cep/testCEP.java
+++ src/test/java/org/wso2/carbon/cep/testCEP.java
@@ -1,5 +1,6 @@
 package org.wso2.carbon.cep;
 
+import junit.framework.Assert;
 import org.junit.Test;
 
 import java.util.Properties;
@@ -11,8 +12,6 @@ public class testCEP {
         LocalCEP localCEP = new LocalCEP();
         Properties properties = new Properties();
         properties.put("name","API1");
-        localCEP.addThrottlingType(ThrottlingManager.ThrottlingType.Rule1, properties);
-        localCEP.addThrottlingType(ThrottlingManager.ThrottlingType.Rule2, properties);
         localCEP.init();
     }
 
@@ -24,5 +23,20 @@ public class testCEP {
         remoteCEP.addThrottlingType(ThrottlingManager.ThrottlingType.Rule1, properties);
         remoteCEP.addThrottlingType(ThrottlingManager.ThrottlingType.Rule2, properties);
         remoteCEP.init();
+    }
+
+    @Test
+    public void testSynchronousBehaviour() throws InterruptedException {
+        final ResultContainer result = new ResultContainer(2);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Assert.assertTrue("Assert the throttling result", result.isThrottled());
+            }
+        });
+        thread.start();
+        Thread.sleep(1000);
+        result.addResult(true);
+        result.addResult(false);
     }
 }
